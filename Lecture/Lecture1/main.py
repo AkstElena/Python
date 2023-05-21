d173f9a89b15ac2acc9c994fe87f62f2e7388133
Akst Elena <akstelena@yandex.ru>
2023-05-21 16:34:19 +0300

    First Lecture, seminar, homework

---
+++ Lecture/Lecture1/main.py
@@ -0,0 +1,113 @@
+n = 5
+print(n)
+print(type(n))
+
+'''
+n = None
+print(n)
+print(type(n))
+
+n = 1.89
+print(n)
+print(type(n))
+
+n = "String"
+print(n)
+print(type(n))
+
+n = 'String'
+print(n)
+print(type(n))
+
+n = 'Str\'ing'
+print(n)
+print(type(n))
+
+n = 'Str"proba"ing'
+print(n)
+print(type(n))
+
+n = 'Str"proba ing'
+print(n)
+print(type(n))
+
+n = "Str proba'ing"
+print(n)
+print(type(n))
+'''
+
+'''
+так можно комментировать
+'''
+
+"""
+и так можно комментировать
+"""
+
+# и так 
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(a, b, c)
+# print(a, ' - ',b, '- ', c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# # print('Введите первое число: ')
+# # a=int(input())
+
+# # b = int(input ('Введите второе число: '))
+
+# # print(a, '+', b, '=', a+b)
+
+
+# c = 5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+a = 5.897
+b = 5.777
+print(round(a*b,3))
+
+i = 3
+i += 3 # =i+3
+i /= 3 # =i/3
+
+# username = input("Введите имя: ")
+# if username == "Маша":
+#   print("Ура, это Маша!")
+# else:
+#   print(f'Привет, {username}!')
+
+# нахождение минимального делителя
+# n = int (input())
+# flag = True
+# i = 2
+# while flag:
+#   if n % i == 0:
+#     flag = False
+#     print(i)
+#   elif i > n // 2:
+#     print(n)
+#     flag = False
+#   i +=1
+
+
+# пример перебора значений
+for i in 1, 8, 10:
+  print(i)
+
+
+r = range(100,0,-20)
+for i in r:
+  print(i)
+
+
+a = 'qwerty'
+for i in a:
+  print(i)
+
+
